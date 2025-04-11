147accf697c7b28b50b8d7223a75a34e8275c253
david <miharisoadavidfils@gmail.com>
2025-04-11 17:02:13 +0300

    [ADD] structure of the folders and configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/pages/**/*.html",
+    "./src/assets/js/**/*.js"  // Chemin modifié pour vos fichiers JS
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#10B981'
+      }
+    },
+  },
+  plugins: [],
+}
+s
