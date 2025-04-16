def2a9c63c2235d6f15d782981a1f62f7ea234d7
david <miharisoadavidfils@gmail.com>
2025-04-16 09:29:38 +0300

    [UPADTE] header

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,18 +13,6 @@ export default {
         'primary_light': '#d0dddd',
         'red_color': '#f93b3b',
         'green_color': '#1cc74f'
-      },
-      boxShadow: {
-        'top': '0 -4px -10px 10px rgba(0, 0, 0, 0.1)',
-        'right': '4px 0 6px -1px rgba(0, 0, 0, 0.1)',
-        'bottom': '0 4px 6px -1px rgba(0, 0, 0, 0.1)',
-        'left': '-4px 0 6px -1px rgba(0, 0, 0, 0.1)',
-        'all-sides': [
-          '-4px 0 6px -1px rgba(0, 0, 0, 0.1)',
-          '4px 0 6px -1px rgba(0, 0, 0, 0.1)',
-          '0 -4px 10px 10px rgba(0, 0, 0, 0.1)',
-          '0 4px 6px -1px rgba(0, 0, 0, 0.1)'
-        ],
       }
     },
   },
